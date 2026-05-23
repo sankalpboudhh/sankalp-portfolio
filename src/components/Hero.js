bb19a85c7ef9c8b0941301df889d42d54fc6e653
Sankalp Boudhh <sankalpboudhh@gmail.com>
2026-05-23 14:32:35 +0530

    updating get in touch action

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -32,7 +32,7 @@ function Hero() {
             <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
           </svg>
         </a>
-        <a href="mailto:sankalpboudhh@gmail.com" className="social-icon">
+        <a href="#contact" className="social-icon">
           <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20">
             <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 010 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"/>
           </svg>
@@ -50,7 +50,7 @@ function Hero() {
           </p>
           <div className="hero-ctas">
             <a href="#projects" className="btn-primary">View my work →</a>
-            <a href="mailto:sankalpboudhh@gmail.com" className="btn-outline">Get in touch</a>
+            <a href="#contact" className="btn-outline">Get in touch</a>
           </div>
         </div>
 
@@ -70,7 +70,7 @@ function Hero() {
           <div className="stat-label">Years experience</div>
         </div>
         <div className="stat">
-          <div className="stat-num">800+</div>
+          <div className="stat-num">100+</div>
           <div className="stat-label">Dealerships served</div>
         </div>
         <div className="stat">
